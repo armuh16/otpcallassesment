1e89fd242c4d67212a20382a2386a0cf4dff0dec
Darkshad0w <65877605+armuh16@users.noreply.github.com>
2022-02-21 01:18:07 +0800

    Call Otp

--- main.go
+++ main.go
@@ -1,45 +1,37 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"html/template"
-	"log"
-	"net/http"
-)
-
 func main() {
-	var countries []Country
-	response, err := http.Get("https://citcall.com/test/countries.json")
-	if err != nil {
-		log.Fatalln(err)
-	}
+	// 	var countries []Country
+	// 	response, err := http.Get("https://citcall.com/test/countries.json")
+	// 	if err != nil {
+	// 		log.Fatalln(err)
+	// 	}
 
-	decoder := json.NewDecoder(response.Body)
-	decoder.DisallowUnknownFields()
+	// 	decoder := json.NewDecoder(response.Body)
+	// 	decoder.DisallowUnknownFields()
 
-	err = decoder.Decode(&countries)
-	if err != nil {
-		log.Fatalln("err decoding body")
-	}
+	// 	err = decoder.Decode(&countries)
+	// 	if err != nil {
+	// 		log.Fatalln("err decoding body")
+	// 	}
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		JSONtoTable(w, countries)
-	})
+	// 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 		JSONtoTable(w, countries)
+	// 	})
 
-	fmt.Println("server started at localhost:9000")
-	http.ListenAndServe(":9000", nil)
-}
+	// 	fmt.Println("server started at localhost:9000")
+	// 	http.ListenAndServe(":9000", nil)
+	// }
 
-func JSONtoTable(w http.ResponseWriter, data []Country) {
-	t := template.New("t")
-	t, err := t.Parse(templateString)
-	if err != nil {
-		panic(err)
-	}
+	// func JSONtoTable(w http.ResponseWriter, data []Country) {
+	// 	t := template.New("t")
+	// 	t, err := t.Parse(templateString)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
 
-	err = t.Execute(w, data)
-	if err != nil {
-		panic(err)
-	}
+	// 	err = t.Execute(w, data)
+	// 	if err != nil {
+	// 		panic(err)
+	// 	}
 }
